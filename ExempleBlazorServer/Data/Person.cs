386406750e09078c6b47e0df09f958f9c26ffb4d
ihab <ihab@macbook-pro-de-ihab.home>
2021-06-01 14:22:55 +0200

    formation blazor

---
+++ ExempleBlazorServer/Data/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExempleBlazorServer.Data
+{
+    public class Person
+    {
+        public Person()
+        {
+        }
+
+        private string firstName;
+
+        private string lastName;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+
+
+        public static List<Person> GetPersons()
+        {
+            return new List<Person>() { new Person { FirstName = "toto", LastName = "tata" }, new Person { FirstName = "titi", LastName = "minet" }, new Person { FirstName = "toto", LastName = "tata" } };
+        }
+    }
+}
