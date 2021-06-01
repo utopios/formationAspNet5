9a5145a2fe510338853d9a79507ee55667cd1192
ihab <ihab@macbook-pro-de-ihab.home>
2021-06-01 15:27:33 +0200

    suite formation blazor

---
+++ blazorWebProgressif/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace blazorWebProgressif.Models
+{
+    public class Person
+    {
+        public Person()
+        {
+        }
+
+        private string firstName;
+        private string lastName;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+    }
+}
