f326c68555149f53fe0b2969d509b55098d9b9df
ihab <ihab@macbook-pro-de-ihab.home>
2021-06-02 10:30:04 +0200

    formation api rest

---
+++ FormationApiRest/Controllers/PersonController.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FormationApiRest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormationApiRest.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            //return await Task<List<Person>>.Run(() =>  new List<Person>() { new Person { FirstName = "toto", LastName = "tata" }, new Person { FirstName = "titi", LastName = "minet" } });
+
+            List<Person> peoples = await Person.GetPersonAsync();
+
+            return Ok(peoples);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await Person.GetPersonAsyncById(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] PersonRecord person)
+        {
+            return Ok(await Person.PostPersonAsync(person));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromBody] PersonRecord person, int id)
+        {
+            Person personEdit = await Person.GetPersonAsyncById(id);
+            if(personEdit == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                if (personEdit.Update(person))
+                {
+                    return Ok(new { message = "update ok" });
+                }
+                else
+                {
+                    return Ok(new { message = "error update" });
+                }
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await Person.DeletePersonAsync(id));
+        }
+    }
+
+}
---
+++ FormationApiRest/Models/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace FormationApiRest.Models
+{
+    public class Person
+    {
+        string firstName;
+        string lastName;
+        int id;
+        public Person()
+        {
+        }
+
+        public string FirstName { get => firstName; set { if (value.Length > 3) firstName = value; else throw new FormatException("Merci de saisir un prénom min 3 caractère"); } }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int Id { get => id; set => id = value; }
+
+        public static Task<List<Person>> GetPersonAsync()
+        {
+            Task<List<Person>> TaskPerson = Task.Run(() =>
+            {
+                //Logic metier
+                return new List<Person>() {
+                    new Person { FirstName = "toto", LastName = "tata" }, new Person { FirstName = "titi", LastName = "minet" }
+                };
+            });
+            
+            return TaskPerson;
+        }
+
+        public static Task<Person> GetPersonAsyncById(int id)
+        {
+            return Task.Run(() =>
+            {
+                //Logic metier
+                return new Person { FirstName = "ihab", LastName = "abadi", Id = id };
+            });
+        }
+
+        public static Task<Person> PostPersonAsync(PersonRecord person)
+        {
+            return Task.Run(() =>
+            {
+                return new Person { Id = new Random().Next(100), FirstName = person.FirstName, LastName = person.LastName };
+            });
+        }
+        public static Task<bool> DeletePersonAsync(int id)
+        {
+            return Task.Run(() =>
+            {
+                return true;
+            });
+        }
+
+        public bool Update(PersonRecord person)
+        {
+            //Mise à jour dans la base.
+            FirstName = person.FirstName;
+            LastName = person.LastName;
+            return true;
+        }
+    }
+
+    public record PersonRecord(string FirstName, string LastName);
+
+}
